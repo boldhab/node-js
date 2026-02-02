92cb8b90668fab0a18c81fd7336e2c1c01ab1849
Habtamu Befekadu <habtamubefekadu20@gmail.com>
2026-02-02 21:36:14 +0300

    add initial implementation of readline interface for user input

---
+++ node js basic/app.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Please enter your name: ", (name) => {
+    console.log("You entered the name: " + name);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log("Interface is closed");
+    process.exit(0);
+});
