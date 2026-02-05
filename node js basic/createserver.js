8306e4fc979944a0773403dd32df1b8cef31fc5b
Habtamu Befekadu <habtamubefekadu20@gmail.com>
2026-02-05 20:52:03 +0300

    add: implement server response with HTML content and add initial HTML, CSS, and JavaScript files

--- node js basic/createserver.js
+++ node js basic/createserver.js
@@ -1,7 +1,10 @@
 const http=require('http');
+const fs=require('fs');
+
+const html=fs.readFileSync('./templete/index.html','utf-8');
 const server=http.createServer((request,response)=>{
     console.log("request recived");
-    response.end("Hello from the server");
+    response.end(html);
 });
 server.listen(8000,'127.0.0.1',()=>{
     console.log("server is listening on port 8000");
