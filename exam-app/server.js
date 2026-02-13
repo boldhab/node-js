0087aec21854a587141568fee29d95a7ed88dd62
Habtamu Befekadu <habtamubefekadu20@gmail.com>
2026-02-13 14:53:00 +0300

    feat: add routes for serving login page

--- exam-app/server.js
+++ exam-app/server.js
@@ -12,6 +12,12 @@ app.use(express.static('public'));//to access static files
 app.use(logger);
 
 app.use('/api',examroutes);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
